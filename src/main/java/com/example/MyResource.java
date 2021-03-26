17e9428348ee1346c7a4c6969681a03b8fd7722d
Aldayr <aldayr.ruiz@opendeusto.es>
2021-03-26 09:17:14 +0100

    Delete lines MyResource

--- src/main/java/com/example/MyResource.java
+++ src/main/java/com/example/MyResource.java
@@ -5,21 +5,4 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-/**
- * Root resource (exposed at "myresource" path)
- */
-@Path("myresource")
-public class MyResource {
 
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Hello World";
-    }
-}
